4b0bb94aa7bba54235b02f7f3fefe9152f88e4b3
Masaki Haruka <yek@reasonset.net>
2025-06-23 00:21:20 +0900

    Add character specific words

---
+++ util/cat.zsh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+sed '/^\s*$/ d' src/* >| ww.txt
--- util/checkdict.rb
+++ util/checkdict.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby
 
 ARGF.each do |line|
-  unless line =~ /^[\p{Hiragana}ー]+\t.*?\t(?:人名|地名|組織|固有名詞|名詞)\t/
+  unless line =~ /^[\p{Hiragana}ー]+\t.*?\t(?:人名|地名|組織|固有名詞|名詞)\t[^\t]*$/
     p line
   end
 end
