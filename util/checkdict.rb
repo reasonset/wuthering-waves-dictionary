d9c97afb454b6a2e5aade9b1a332c6e340bb8ea2
Masaki Haruka <yek@reasonset.net>
2025-09-20 11:35:38 +0900

    Update receipt words

--- util/checkdict.rb
+++ util/checkdict.rb
@@ -8,8 +8,8 @@ ARGF.each do |line|
   elem = line.split("\t")
   e0 = elem[0].unicode_normalize(:nfkc)
   e1 = elem[1].unicode_normalize(:nfkc)
-  puts "#{elem[0]} -> #{e0}" if elem[0] != e0
-  puts "#{elem[1]} -> #{e1}" if elem[1] != e1
+  puts "#{elem[0]} -> #{e0}" if elem[0].delete("!?！？") != e0.delete("!?！？")
+  puts "#{elem[1]} -> #{e1}" if elem[1].delete("!?！？") != e1.delete("!?！？")
   if elem[1] =~ /[\u{20000}-\u{2FFFF}]/
     puts "Range #{elem[1]}"
   end
