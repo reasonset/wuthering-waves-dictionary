efac506b40dcf88dd98a7922997bf82b437a9b33
Masaki Haruka <yek@reasonset.net>
2025-11-19 21:40:36 +0900

    Improved the classification

--- util/checkdict.rb
+++ util/checkdict.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby
 
 ARGF.each do |line|
-  unless line =~ /^[\p{Hiragana}ー]+\t.*?\t(?:人名|地名|組織|固有名詞|名詞)\t[^\t]*$/
+  unless line =~ /^[\p{Hiragana}ー]+\t.*?\t(?:人名|地名|組織|固有名詞|名詞|姓|名|短縮よみ)\t[^\t]*$/
     p line
   end
   
